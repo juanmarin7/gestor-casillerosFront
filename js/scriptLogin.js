5d83da47af6903d52722f5a9297b9c9b6b017742
Juan Esteban Cano Marin <juanestebanc6@gmail.com>
2025-03-06 21:54:21 -0500

    Consumo endpoints /registro /login

--- js/scriptLogin.js
+++ js/scriptLogin.js
@@ -1,27 +1,26 @@
-document.getElementById("registroForm").addEventListener("submit", function(event) {
+document.getElementById("loginForm").addEventListener("submit", async function(event) {
     event.preventDefault();
-    
+
     let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    let confirmPassword = document.getElementById("confirmPassword").value;
-    let telefono = document.getElementById("telefono").value;
-    let terms = document.getElementById("terms").checked;
+    let contrasena = document.getElementById("contrasena").value;
 
-    if (password !== confirmPassword) {
-        alert("Las contraseñas no coinciden");
-        return;
+    const response = await fetch("http://localhost:8097/usuarios/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        // body: JSON.stringify({ email, contrasena ,nombre, tipoDocumento, 
+        //     numeroDocumento,fechaNacimiento, pais, ciudad, direccion, telefono  })
+        body: JSON.stringify({ email, contrasena   })
+        
+    });
+
+    const mensaje = document.getElementById("mensaje");
+    if (response.ok) {
+        mensaje.innerText = "Usuario Logeado exitosamente";
+        mensaje.style.color = "green";
+    } else {
+        mensaje.innerText = "Error al ingresar ";
+        mensaje.style.color = "red";
     }
-
-    if (!/^[\d]{10}$/.test(telefono)) {
-        alert("El teléfono debe contener 10 dígitos");
-        return;
-    }
-
-    if (!terms) {
-        alert("Debe aceptar los términos y condiciones");
-        return;
-    }
-
-    alert("Registro exitoso!");
-    this.reset();
 });
--- js/scriptRegistro.js
+++ js/scriptRegistro.js
@@ -7,30 +7,54 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 
-document.getElementById("registroForm").addEventListener("submit", function(event) {
+document.getElementById("registroForm").addEventListener("submit", async function(event) {
     event.preventDefault();
-    
-    //let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    let confirmPassword = document.getElementById("confirmPassword").value;
-    let telefono = document.getElementById("telefono").value;
-    let terms = document.getElementById("terms").checked;
 
-    if (password !== confirmPassword) {
+    let email = document.getElementById("email").value;
+    let contrasena = document.getElementById("contrasena").value;
+    let confirmPassword = document.getElementById("confirmPassword").value;
+    let nombre = document.getElementById("nombres").value;
+    let tipoDocumento = document.getElementById("tipoDocumento").value;
+    let numeroDocumento = document.getElementById("numeroDocumento").value;
+    let fechaNacimiento = document.getElementById("fechaNacimiento").value;
+    let pais = document.getElementById("pais").value;
+    let ciudad = document.getElementById("ciudad").value;
+    let direccion = document.getElementById("direccion").value;
+    let telefono = document.getElementById("telefono").value;
+
+    numeroDocumento
+    if (contrasena !== confirmPassword) {
         alert("Las contraseñas no coinciden");
         return;
     }
 
-    if (!/^[\d]{10}$/.test(telefono)) {
-        alert("El teléfono debe contener 10 dígitos");
-        return;
-    }
+    // if (!/^[\d]{10}$/.test(telefono)) {
+    //     alert("El teléfono debe contener 10 dígitos");
+    //     return;
+    // }
 
     if (!terms) {
         alert("Debe aceptar los términos y condiciones");
         return;
     }
 
-    alert("Registro exitoso!");
-    this.reset();
+    const response = await fetch("http://localhost:8097/usuarios/registrar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        // body: JSON.stringify({ email, contrasena ,nombre, tipoDocumento, 
+        //     numeroDocumento,fechaNacimiento, pais, ciudad, direccion, telefono  })
+        body: JSON.stringify({ email, contrasena ,nombre  })
+        
+    });
+
+    const mensaje = document.getElementById("mensaje");
+    if (response.ok) {
+        mensaje.innerText = "Usuario registrado exitosamente";
+        mensaje.style.color = "green";
+    } else {
+        mensaje.innerText = "Error al registrar usuario";
+        mensaje.style.color = "red";
+    }
 });
